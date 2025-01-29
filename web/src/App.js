6cef5365b95d7f1d989e620f1ebd6cf1bebfa197
test <test@qq.com>
2025-01-29 18:19:40 +0800

    feat: refine

--- web/src/App.js
+++ web/src/App.js
@@ -2,9 +2,18 @@ import React, { useState } from 'react';
 import { useNavigate, Routes, Route } from 'react-router';
 import { Layout, Menu, theme } from 'antd';
 import Home from './pages/Home';
-import TestA from './pages/TestA';
-import TestB from './pages/TestB';
 import RrelayControl from './pages/RelayControl';
+
+import Page01 from './pages/Page01';
+import Page02 from './pages/Page02';
+import Page03 from './pages/Page03';
+import Page04 from './pages/Page04';
+import Page05 from './pages/Page05';
+import Page06 from './pages/Page06';
+import Page07 from './pages/Page07';
+import Page08 from './pages/Page08';
+import Page09 from './pages/Page09';
+import Page10 from './pages/Page10';
 
 const { Content, Sider } = Layout;
 
@@ -14,9 +23,19 @@ function getItem(label, key, icon, children) {
 
 // 左侧导航
 const items = [
-  getItem('首页', '/'),
-  getItem('测试', '/test'),
-  getItem('RELAY_CONTROL', '/relay_control'),
+  getItem('HOME', '/'),
+  getItem('OUTPUT', '/relay_control'),
+
+  getItem('PAGE 1', '/page_01'),
+  getItem('PAGE 2', '/page_02'),
+  getItem('PAGE 3', '/page_03'),
+  getItem('PAGE 4', '/page_04'),
+  getItem('PAGE 5', '/page_05'),
+  getItem('PAGE 6', '/page_06'),
+  getItem('PAGE 7', '/page_07'),
+  getItem('PAGE 8', '/page_08'),
+  getItem('PAGE 9', '/page_09'),
+  getItem('PAGE 10', '/page_10'),
 ];
 
 const App = () => {
@@ -28,6 +47,7 @@ const App = () => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="logo">HANKERILA</div>
         <Menu
           theme="dark"
           defaultSelectedKeys={['1']}
@@ -52,9 +72,18 @@ const App = () => {
             <Routes>
               {/* 路由配置 */}
               <Route index element={<Home />} />
-              <Route path="/test" element={<TestA />} />
-              <Route path="/test/b" element={<TestB />} />
               <Route path="/relay_control" element={<RrelayControl />} />
+
+              <Route path="/page_01" element={<Page01 />} />
+              <Route path="/page_02" element={<Page02 />} />
+              <Route path="/page_03" element={<Page03 />} />
+              <Route path="/page_04" element={<Page04 />} />
+              <Route path="/page_05" element={<Page05 />} />
+              <Route path="/page_06" element={<Page06 />} />
+              <Route path="/page_07" element={<Page07 />} />
+              <Route path="/page_08" element={<Page08 />} />
+              <Route path="/page_09" element={<Page09 />} />
+              <Route path="/page_10" element={<Page10 />} />
             </Routes>
           </div>
         </Content>
---
+++ web/src/pages/Page01.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { Space, Button } from 'antd';
+
+const Component = () => {
+    useEffect(() => {
+        // 初始化
+    }, [])
+
+    const onClick = () => {
+        fetch("/action", {
+            "body": {
+                
+            },
+            "method": "POST"
+        }).then(resp => resp.json()).then(console.info);
+    };
+
+    return (
+        <div>
+            <div>Page01</div>
+            <Space>
+                <Button onClick={onClick}>1</Button>
+            </Space>
+        </div>
+    )
+};
+
+export default Component;
